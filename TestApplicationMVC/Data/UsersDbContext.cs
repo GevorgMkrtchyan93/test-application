1c595e1eeb089e89d794274d05f5445f09c5076e
Gev123 <gevorg.mkrtchyan@milies.net>
2022-05-31 11:56:20 +0400

    Authentication

---
+++ TestApplicationMVC/Data/UsersDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApplicationMVC.Models;
+
+namespace TestApplicationMVC.Data
+{
+    public class UsersDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
